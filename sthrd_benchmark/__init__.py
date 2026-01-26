36c14cc4c0fa4dc47afbbca49fbe37157b93975e
Pavel Averin <pasha2281337alfa@gmail.com>
2026-01-26 09:00:37 +0300

    feat: add the first version with visualization

---
+++ sthrd_benchmark/__init__.py
@@ -0,0 +1,5 @@
+from .core import STHRDBenchmark, AbstractOptimizer, BenchmarkEnvironment
+from .visualizer import BenchmarkVisualizer
+
+__version__ = "1.0.0"
+__all__ = ['STHRDBenchmark', 'AbstractOptimizer', 'BenchmarkEnvironment', 'BenchmarkVisualizer']
